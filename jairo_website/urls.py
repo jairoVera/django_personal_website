54477dc5cc57a668e99030816b430147934fc10e
jairo_vera <jairo.vera.1994@gmail.com>
2017-05-17 13:20:56 -0500

    added nav bar and an about html page

--- jairo_website/urls.py
+++ jairo_website/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'jairo_website'
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^about/$', views.about, name = 'about'),
 ]
--- jairo_website/views.py
+++ jairo_website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, 'jairo_website/index.html')
+
+def about(request):
+    return render(request, 'jairo_website/about.html')
